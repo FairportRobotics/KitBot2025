99b7794c6de3f5b68a3147d1346fb85c9219a3d4
Ray Healy <ray.healy@fairportrobotics.org>
2025-09-24 19:54:58 -0400

    Swap motor controllers

--- src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -8,6 +8,9 @@ import org.fairportrobotics.frc.posty.TestableSubsystem;
 import org.fairportrobotics.frc.posty.test.PostTest;
 import static org.fairportrobotics.frc.posty.assertions.Assertions.*;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.WPI_CallbackHelper;
+
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -15,24 +18,25 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import frc.robot.Constants.DriveConstants;
 
 // Class to drive the robot over CAN
 public class CANDriveSubsystem extends TestableSubsystem {
-  private final SparkMax leftLeader;
-  private final SparkMax leftFollower;
-  private final SparkMax rightLeader;
-  private final SparkMax rightFollower;
+  private final WPI_VictorSPX leftLeader;
+  private final WPI_VictorSPX leftFollower;
+  private final WPI_VictorSPX rightLeader;
+  private final WPI_VictorSPX rightFollower;
 
   private final DifferentialDrive drive;
 
   public CANDriveSubsystem() {
     super();
     // create brushed motors for drive
-    leftLeader = new SparkMax(DriveConstants.LEFT_LEADER_ID, MotorType.kBrushed);
-    leftFollower = new SparkMax(DriveConstants.LEFT_FOLLOWER_ID, MotorType.kBrushed);
-    rightLeader = new SparkMax(DriveConstants.RIGHT_LEADER_ID, MotorType.kBrushed);
-    rightFollower = new SparkMax(DriveConstants.RIGHT_FOLLOWER_ID, MotorType.kBrushed);
+    leftLeader = new WPI_VictorSPX(DriveConstants.LEFT_LEADER_ID);
+    leftFollower = new WPI_VictorSPX(DriveConstants.LEFT_FOLLOWER_ID);
+    rightLeader = new WPI_VictorSPX(DriveConstants.RIGHT_LEADER_ID);
+    rightFollower = new WPI_VictorSPX(DriveConstants.RIGHT_FOLLOWER_ID);
 
     // set up differential drive class
     drive = new DifferentialDrive(leftLeader, rightLeader);
@@ -40,10 +44,10 @@ public class CANDriveSubsystem extends TestableSubsystem {
     // Set can timeout. Because this project only sets parameters once on
     // construction, the timeout can be long without blocking robot operation. Code
     // which sets or gets parameters during operation may need a shorter timeout.
-    leftLeader.setCANTimeout(250);
-    rightLeader.setCANTimeout(250);
-    leftFollower.setCANTimeout(250);
-    rightFollower.setCANTimeout(250);
+    //leftLeader.setCANTimeout(250);
+    //rightLeader.setCANTimeout(250);
+    //leftFollower.setCANTimeout(250);
+    //rightFollower.setCANTimeout(250);
 
     // Create the configuration to apply to motors. Voltage compensation
     // helps the robot perform more similarly on different
@@ -54,21 +58,8 @@ public class CANDriveSubsystem extends TestableSubsystem {
     config.voltageCompensation(12);
     config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
 
-    // Set configuration to follow leader and then apply it to corresponding
-    // follower. Resetting in case a new controller is swapped
-    // in and persisting in case of a controller reset due to breaker trip
-    config.follow(leftLeader);
-    leftFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    config.follow(rightLeader);
-    rightFollower.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    // Remove following, then apply config to right leader
-    config.disableFollowerMode();
-    rightLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // Set conifg to inverted and then apply to left leader. Set Left side inverted
-    // so that postive values drive both sides forward
-    config.inverted(true);
-    leftLeader.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftFollower.follow(this.leftLeader);
+    rightFollower.follow(this.rightLeader);
   }
 
   @Override
